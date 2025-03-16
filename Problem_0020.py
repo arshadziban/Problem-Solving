1ceb4c39ee39c96f4ee303ace359a58fc144c3e4
arsahdziban <arshadziban031201@gmail.com>
2025-03-16 20:21:11 +0600

    initail commit

---
+++ Problem_0020.py
@@ -0,0 +1,10 @@
+# swaping 2 number without 3rd Variable
+a = int (input("Enter the first number= "))
+b = int (input("Enter the second number= "))
+
+print ("Before swaping a=",a,"b=",b)
+
+
+a,b = b,a
+
+print ("After swaping a=",a,"b=",b)
