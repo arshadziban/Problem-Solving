6354f8217c4292c490b9456ae4d6fb44ec827afd
arsahdziban <arshadziban031201@gmail.com>
2025-04-26 12:46:40 +0600

    initial commit

---
+++ Problem_0033.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+        return []
+# Example
+solution = Solution()
+print(solution.twoSum([2,7,11,15], 9))  
+print(solution.twoSum([3,2,4], 6))     
+print(solution.twoSum([3,3], 6))        
+print(solution.twoSum([1,2,3,4,5], 8))
